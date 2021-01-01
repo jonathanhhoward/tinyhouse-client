fde2e7e48c4710fc757d9fcf23b5546f2e4e235e
jonathanhhoward <2773262@gmail.com>
2021-01-01 11:36:08 -0600

    Module 9-5

---
+++ src/sections/Listings/components/ListingsSkeleton/index.tsx
@@ -0,0 +1,31 @@
+import { Card, List, Skeleton } from "antd";
+
+import listingLoadingCardCover from "../../assets/listing-loading-card-cover.jpg";
+
+export function ListingsSkeleton() {
+  const emptyData = [{}, {}, {}, {}, {}, {}, {}, {}];
+
+  return (
+    <div>
+      <Skeleton paragraph={{ rows: 1 }} />
+      <List
+        grid={{ gutter: 8, xs: 1, sm: 2, md: 2, lg: 4, xl: 4, xxl: 4 }}
+        dataSource={emptyData}
+        renderItem={() => (
+          <List.Item>
+            <Card
+              cover={
+                <div
+                  style={{ backgroundImage: `url(${listingLoadingCardCover})` }}
+                  className="listings-skeleton__card-cover-img"
+                />
+              }
+              loading
+              className="listings-skeleton__card"
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+}
--- src/sections/Listings/index.tsx
+++ src/sections/Listings/index.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import { Layout, List, Typography } from "antd";
-import { ListingCard } from "../../lib/components";
+import { Affix, Layout, List, Typography } from "antd";
+import { ErrorBanner, ListingCard } from "../../lib/components";
 import { LISTINGS } from "../../lib/graphql/queries";
 import {
   Listings as ListingsData,
   ListingsVariables,
 } from "../../lib/graphql/queries/Listings/__generated__/Listings";
 import { ListingsFilter } from "../../lib/graphql/__generated__/globalTypes";
+import {
+  ListingsFilters,
+  ListingsPagination,
+  ListingsSkeleton,
+} from "./components";
 
 interface MatchParams {
   location: string;
@@ -19,14 +25,20 @@ const { Paragraph, Text, Title } = Typography;
 const PAGE_LIMIT = 8;
 
 export function Listings({ match }: RouteComponentProps<MatchParams>) {
-  const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
-    variables: {
-      location: match.params.location,
-      filter: ListingsFilter.PRICE_LOW_TO_HIGH,
-      limit: PAGE_LIMIT,
-      page: 1,
-    },
-  });
+  const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
+  const [page, setPage] = useState(1);
+
+  const { data, loading, error } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        location: match.params.location,
+        filter,
+        limit: PAGE_LIMIT,
+        page,
+      },
+    }
+  );
 
   const listings = data?.listings;
   const listingsRegion = listings?.region;
@@ -38,15 +50,26 @@ export function Listings({ match }: RouteComponentProps<MatchParams>) {
   ) : null;
 
   const listingsSectionElement = listings?.result.length ? (
-    <List
-      grid={{ gutter: 8, xs: 1, sm: 2, md: 2, lg: 4, xl: 4, xxl: 4 }}
-      dataSource={listings.result}
-      renderItem={(listing) => (
-        <List.Item>
-          <ListingCard listing={listing} />
-        </List.Item>
-      )}
-    />
+    <div>
+      <Affix offsetTop={64}>
+        <ListingsPagination
+          total={listings.total}
+          page={page}
+          limit={PAGE_LIMIT}
+          setPage={setPage}
+        />
+        <ListingsFilters filter={filter} setFilter={setFilter} />
+      </Affix>
+      <List
+        grid={{ gutter: 8, xs: 1, sm: 2, md: 2, lg: 4, xl: 4, xxl: 4 }}
+        dataSource={listings.result}
+        renderItem={(listing) => (
+          <List.Item>
+            <ListingCard listing={listing} />
+          </List.Item>
+        )}
+      />
+    </div>
   ) : (
     <div>
       <Paragraph>
@@ -60,7 +83,21 @@ export function Listings({ match }: RouteComponentProps<MatchParams>) {
     </div>
   );
 
-  return (
+  return loading ? (
+    <Content className="listings">
+      <ListingsSkeleton />
+    </Content>
+  ) : error ? (
+    <Content className="listings">
+      <ErrorBanner
+        description={`
+            We either couldn't find anything matching your search or have encountered an error.
+            If you're searching for a unique location, try searching again with more common keywords.
+          `}
+      />
+      <ListingsSkeleton />
+    </Content>
+  ) : (
     <Content className="listings">
       {listingsRegionElement}
       {listingsSectionElement}
